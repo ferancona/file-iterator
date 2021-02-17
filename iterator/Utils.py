20b91dfb1b83e153f79e681d24e3faff69121820
Fernando Ancona <fernando.ancona@verato.com>
2021-02-16 22:20:14 -0600

    feat - ReadEvents, ExceptionRaiser, FileManager

---
+++ iterator/Utils.py
@@ -0,0 +1,32 @@
+import events
+import gzip, zipfile
+
+
+class ReadEvents(events.Events):
+    __events__ = ('on_start_file_reading', 'on_stop_file_reading', 
+                  'on_end_file_reached', 'on_end_reached')
+
+    @property
+    def file_events(self):
+        return ('on_start_file_reading', 'on_stop_file_reading', 
+                'on_end_file_reached')
+
+
+class ExceptionRaiser:
+    def __init__(self, exception):
+        super().__init__()
+        self.ex = exception
+    def raise_ex(self):
+        raise self.ex
+
+
+class FileManager:
+    @classmethod
+    def is_gzipfile(cls, filepath):
+        # First bytes of gzip files are: 1f 8b.
+        FIRST_BYTES = '1f8b'
+        return open(filepath).read(2).encode('hex') == FIRST_BYTES
+
+    @classmethod
+    def is_zipfile(cls, filepath):
+        return zipfile.is_zipfile(filepath)
