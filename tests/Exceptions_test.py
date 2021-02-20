51ec383b40b007ab3dff4b323557f537b824708a
Fernando Ancona <fernando.ancona@verato.com>
2021-02-20 00:18:09 -0600

    test - Add Exceptions test

---
+++ tests/Exceptions_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from iterator.Exceptions import ExceptionRaiser
+
+
+@pytest.fixture
+def stop_iteration_raiser():
+    return ExceptionRaiser(StopIteration)
+
+def test_raise_exception(stop_iteration_raiser):
+    with pytest.raises(StopIteration):
+        stop_iteration_raiser.raise_ex()
