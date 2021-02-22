6107d68dce052db07a1646cd001cb51b3f7f34d2
Fernando Ancona <fernando.ancona@verato.com>
2021-02-21 22:39:02 -0600

    feat - copy() for ReadEvents and _EventSlot

--- iterator/Utils.py
+++ iterator/Utils.py
@@ -11,6 +11,30 @@ class ReadEvents(events.Events):
         return ('on_start_file_reading', 'on_stop_file_reading', 
                 'on_end_file_reached')
 
+    def copy(self, omit=None):
+        # Copy callable attributes.
+        copy = self.__class__()
+        for attr in dir(self):
+            if not attr.startswith('__'):
+                if callable(getattr(self, attr)):
+                    if getattr(self, attr) not in omit:
+                        setattr(copy, attr, 
+                                getattr(self, attr).copy(omit)
+                        )
+        return copy
+
+
+class Read_EventSlot(events.events._EventSlot):
+    def copy(self, omit=None):
+        copy = self.__class__()
+        omit = set(omit) if omit else set()
+        for target in self.targets:
+            if target not in omit:
+                copy.targets.append(target)
+            # copy.targets = self.targets
+        copy.__name__ = self.__name__
+        return copy
+
 
 class FileManager:
     @classmethod
