8db3af885dcf7b096b4c992ac91092f90ac8dff3
Fernando Ancona <fernando.ancona@verato.com>
2021-03-06 12:47:11 -0600

    chore - author info, keywords, classifiers

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup, find_packages
 
-setup(name='file_iterator',
-    version='0.0.1',
+setup (
+    name='file-iterator',
+    packages=find_packages(),
+    version='1.0.0',
+    license='MIT',
     description='Utilities for iterating file contents.',
-    url='https://github.com/ferancona/file-iterator',
     author='Fernando Ancona',
-    author_email='',
+    author_email='f.anconac@gmail.com',
+    url='https://github.com/ferancona/file-iterator',
+    download_url = '',
+    keywords = ['FILE', 'READ', 'CONTENT', 'ITERATION'],
     install_requires=[
         'events'
     ],
-    packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     zip_safe=False
 )
